e3dbd7c557b41a86269771e07519d651699112e1
eedygreen <idris.isah2@st.futminna.edu.ng>
2021-08-11 15:23:15 +0100

    separate provider from vpc resources

--- resource/resources.tf
+++ resource/resources.tf
@@ -17,14 +17,3 @@ provider "aws" {
   shared_credentials_file = "$HOME/.aws/credentials"
   profile                 = "default"
 }
-
-# resources
-
-resource "aws_vpc" "public_vpc" {
-  cidr_block       = "10.10.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    "Name" = "public_vpc"
-  }
-}
