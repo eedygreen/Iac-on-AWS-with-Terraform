67ce4b9fe5e39eff44ca53c035b2387c44fb668a
Isah Idris <eedygreen@outlook.com>
2021-08-19 12:03:03 +0100

    modified output.tf

--- re_usable_module/output.tf
+++ re_usable_module/output.tf
@@ -1,9 +1,22 @@
-output "apache-webserver-ip" {
-  description = "Public IP address"
-  value       = module.ec2_cluster.*.public_ip # list of ip addresses
+output "S3_arn" {
+  description = "ARN of the S3 Bucket"
+  value       = aws_s3_bucket.s3_bucket.arn 
 }
 
-output "apache-webserver-dns" {
-  description = "DNS address"
-  value       = module.ec2_cluster.*.public_dns # list of dns addresses
+output "S3_name" {
+  description = "The Name of the S3 bucket"
+  value       = aws_s3_bucket.s3_bucket.id
 }
+
+output "S3_domain" {
+  description = "The domain Name of the S3 bucket"
+  value       = aws_s3_bucket.s3_bucket.website_domain
+}
+
+output "S3_endpoint" {
+  description = "The endpoint of the S3 bucket"
+  value       = aws_s3_bucket.s3_bucket.website_endpoint
+}
+
+# for any error refer to the below link
+# https://registry.terraform.io/modules/terraform-aws-modules/s3-bucket/aws/latest?tab=outputs
