2094a4c2b96d2322a8caae2766676bd54afff915
Isah Idris <eedygreen@outlook.com>
2021-09-09 10:28:48 +0100

    Modified ec2_instance and s3

--- import _state/s3_bucket/s3_bucket.tf
+++ import _state/s3_bucket/s3_bucket.tf
@@ -1,5 +1,17 @@
 # the local resource name is an existing name of provisioned resources
 
 resource "aws_s3_bucket" "static-website" {
+    bucket = "eedy-state-bucket"  # this will create tfstate file without the use of terraform import
+    acl = "private"  # if this is not specify the default value in state file will be null
+    force_destroy = true  # setting this to true will allow terraform to delete the resource
     
 }
+
+# use the below command to import the existing resource from cloud
+
+# terraform import aws_s3_bucket.static-website <bucket unique name>
+
+# terraform import aws_s3_bucket.static-website  eedy-state-bucket
+
+# terraform import aws_s3_bucket.bucket eedy-state-bucket
+
